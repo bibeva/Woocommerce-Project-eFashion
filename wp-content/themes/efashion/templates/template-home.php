a074c1be134ee755c8fb661a2cc7b3f423125cfd
Bibek <bibekneva@gmail.com>
2020-03-02 16:55:44 +0000

    Finish working on Woocommerce's categories

--- wp-content/themes/efashion/templates/template-home.php
+++ wp-content/themes/efashion/templates/template-home.php
@@ -38,51 +38,37 @@ get_header(); ?>
   </div>
 </section>
 
-<!-- product category block -->
-<?php
-
-  $taxonomy     = 'product_cat';
-  $orderby      = 'name';  
-  $show_count   = 0;      // 1 for yes, 0 for no
-  $pad_counts   = 0;      // 1 for yes, 0 for no
-  $hierarchical = 1;      // 1 for yes, 0 for no  
-  $title        = '';  
-  $empty        = 0;
-
-  $args = array(
-         'taxonomy'     => $taxonomy,
-         'orderby'      => $orderby,
-         'show_count'   => $show_count,
-         'pad_counts'   => $pad_counts,
-         'hierarchical' => $hierarchical,
-         'title_li'     => $title,
-         'hide_empty'   => $empty
-  );
- $all_categories = get_categories( $args );
- foreach ($all_categories as $cat) {
-    if($cat->category_parent == 0) {
-        $category_id = $cat->term_id;       
-        echo '<br /><a href="'. get_term_link($cat->slug, 'product_cat') .'">'. $cat->name .'</a>';
-
-        $args2 = array(
-                'taxonomy'     => $taxonomy,
-                'child_of'     => 0,
-                'parent'       => $category_id,
-                'orderby'      => $orderby,
-                'show_count'   => $show_count,
-                'pad_counts'   => $pad_counts,
-                'hierarchical' => $hierarchical,
-                'title_li'     => $title,
-                'hide_empty'   => $empty
-        );
-        $sub_cats = get_categories( $args2 );
-        if($sub_cats) {
-            foreach($sub_cats as $sub_category) {
-                echo  $sub_category->name ;
-            }   
-        }
-    }       
-}
-?>
+<!-- product categories block -->
+<section class="block productCategories">
+  <div class="container">
+    <div class="threeCols">
+      <?php
+        $taxonomyName = "product_cat";
+        $prod_categories = get_terms($taxonomyName, array(
+          'orderby'=> 'name',
+          'order' => 'ASC',
+          'hide_empty' => 1
+        ));  
+      
+        foreach( $prod_categories as $prod_cat ) :
+          if ( $prod_cat->parent != 0 )
+            continue;
+            $cat_thumb_id = get_woocommerce_term_meta( $prod_cat->term_id, 'thumbnail_id', true );
+            $cat_thumb_url = wp_get_attachment_image_src( $cat_thumb_id, 'thumbnail-size' )[0];
+            $term_link = get_term_link( $prod_cat, 'product_cat' );
+          ?>
+          <div class="col">
+            <a href="<?php echo $term_link; ?>"> 
+              <div class="image" style="background: url('<?php echo $cat_thumb_url; ?>');">
+                <h3><?php echo $prod_cat->name; ?></h3>
+              </div>
+            </a> 
+          </div>
+        <?php endforeach; 
+        wp_reset_query(); 
+      ?>
+    </div>
+  </div>
+</section>
 
 <?php get_footer(); ?>
